6ddf21414f6a1eb3da2cd19950b8147210f67e30
MohamedHatem303 <mohamedhatm303@gmail.com>
2026-05-07 02:52:59 +0300

    Book

---
+++ src/components/Experience.jsx
@@ -0,0 +1,36 @@
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Book } from "./Book";
+
+export const Experience = () => {
+  return (
+    <>
+      <Book position={[0, 0, 0.64]} />
+      <OrbitControls
+        enableRotate={true}
+        minAzimuthAngle={0}
+        maxAzimuthAngle={0}
+        minPolarAngle={Math.PI / 4}
+        maxPolarAngle={Math.PI / 1.5}
+        enableZoom={true}
+        minDistance={2}
+        maxDistance={8}
+        enablePan={true}
+        panSpeed={1.2}
+        screenSpacePanning={true}
+      />
+      <Environment preset="studio" background={false} />
+      <directionalLight
+        position={[2, 5, 2]}
+        intensity={1.5}
+        castShadow
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        shadow-bias={-0.0001}
+      />
+      <mesh position-y={-1.5} rotation-x={-Math.PI / 2} receiveShadow>
+        <planeGeometry args={[100, 100]} />
+        <shadowMaterial transparent opacity={0.2} />
+      </mesh>
+    </>
+  );
+};
